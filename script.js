dd98730e6efeda83deb48a04de9751975fc8106c
Annie McMahon <anniemcmahon20@gmail.com>
2022-08-12 19:33:51 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let adjectives = ['gross', 'awesome', 'joyful', 'sympathetic', 'arrogant'];
+let nouns = ['boyfriend', 'baby', 'chameleon', 'turtle', 'grandmother'];
+let verbs = ['running', 'tweeting', 'spinning around', 'sobbing', 'giggling'];
+
