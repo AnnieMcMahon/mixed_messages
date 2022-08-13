d923f032ab4fb399e9df00061813ca91cc77ca53
Annie McMahon <anniemcmahon20@gmail.com>
2022-08-13 13:14:45 -0400

    Added README page

---
+++ README.md
@@ -0,0 +1,6 @@
+#Mixed Messages - Mad Lib Generator
+
+This JavaScript program generates a Mad Lib sentence using arrays of random words. 
+
+Author: Annie McMahon
+
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let adjectives = ['gross', 'awesome', 'joyful', 'sympathetic', 'arrogant'];
 let nouns = ['boyfriend', 'baby', 'chameleon', 'turtle', 'grandmother'];
-let verbs = ['running', 'tweeting', 'spinning around', 'sobbing', 'giggling'];
+let verbs = ['running', 'playing', 'spinning around', 'sobbing', 'giggling'];
 let adverbs = ['willingly', 'wildly', 'constantly', 'stupidly', 'lazily']
 
 let random = [];
