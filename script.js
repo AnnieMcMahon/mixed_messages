c9d3d8f6feee67b0917a594de20bf4b07ebcb052
Annie McMahon <anniemcmahon20@gmail.com>
2022-08-13 12:20:16 -0400

    first version

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let adjectives = ['gross', 'awesome', 'joyful', 'sympathetic', 'arrogant'];
 let nouns = ['boyfriend', 'baby', 'chameleon', 'turtle', 'grandmother'];
 let verbs = ['running', 'tweeting', 'spinning around', 'sobbing', 'giggling'];
+let adverbs = ['willingly', 'wildly', 'constantly', 'stupidly', 'lazily']
 
+let random = [];
+let counter = 1;
+
+while (counter < 5) {
+    random.push(Math.floor(Math.random() * 5));
+    counter++;
+}
+
+console.log('My ' + adjectives[random[0]] + ' ' + nouns[random[1]] + ' is ' + verbs[random[2]] + ' ' + adverbs[random[3]] + '.');
