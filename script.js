127f5dbf3420dc633dbf0e7b53f0ead2f0a6d582
Annie McMahon <anniemcmahon20@gmail.com>
2022-08-17 20:24:25 -0400

    Merged branches and saved all changes

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ let adjectives = ['gross', 'awesome', 'joyful', 'sympathetic', 'arrogant'];
 let nouns = ['boyfriend', 'baby', 'chameleon', 'turtle', 'grandmother'];
 let verbs = ['running', 'playing', 'spinning around', 'sobbing', 'giggling'];
 let adverbs = ['willingly', 'wildly', 'constantly', 'stupidly', 'lazily']
-
 let random = [];
-let counter = 1;
 
-while (counter < 5) {
-    random.push(Math.floor(Math.random() * 5));
-    counter++;
-}
-
-const madLibGenerator = (adjective, noun, verb, adverb) => 
+const madLibGenerator = (adjective, noun, verb, adverb) =>
     'My ' + adjective + ' ' + noun + ' is ' + verb + ' ' + adverb + '.';
 
 let buttonElement = document.getElementById('button');
-let messageToDisplay = document.querySelector('p');
-buttonElement.onclick = () =>  messageToDisplay.innerText =
-       madLibGenerator(adjectives[random[0]], nouns[random[1]], verbs[random[2]], adverbs[random[3]]);
+function runMadLib() {
+    random = [];
+    let counter = 1;
+    while (counter < 5) {
+        random.push(Math.floor(Math.random() * 5));
+        counter++;
+    }
+    document.querySelector('p').innerHTML =
+        madLibGenerator(adjectives[random[0]], nouns[random[1]], verbs[random[2]], adverbs[random[3]]);
+}
+buttonElement.onclick = runMadLib;
